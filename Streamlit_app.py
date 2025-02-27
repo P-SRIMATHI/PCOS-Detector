0294e3b06f06a66e2461acdec3499b1106f9ef32
P-SRIMATHI <147895021+P-SRIMATHI@users.noreply.github.com>
2025-02-27 12:55:14 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
 import shap
 import openai
 from sklearn.model_selection import train_test_split
